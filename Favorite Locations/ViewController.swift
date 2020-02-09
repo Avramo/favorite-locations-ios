ba660fb7f7642c0edf8fce713ec5fea5d9bf7923
elnatan <elnatan@ravtech.co.il>
2020-02-09 17:20:14 +0200

    Initial Commit

---
+++ Favorite Locations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Favorite Locations
+//
+//  Created by admin on 09/02/2020.
+//  Copyright © 2020 AM Kirsch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
