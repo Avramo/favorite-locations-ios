d0d338a127b170b0c8e942c4f3c13b6abb2cbc6c
Avraham Kirsch <etrog95@gmail.com>
2020-09-29 15:01:21 +0300

    first commit

---
+++ Favorite Locations/SecondView.swift
@@ -0,0 +1,26 @@
+//
+//  SecondView.swift
+//  Favorite Locations
+//
+//  Created by admin on 09/02/2020.
+//  Copyright © 2020 AM Kirsch. All rights reserved.
+//
+
+import UIKit
+
+
+class SecondView: UIViewController {
+    
+    var userName = "rob"
+    
+    var activeRow = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(userName)
+        print(activeRow)
+    }
+
+
+
+}
--- Favorite Locations/ViewController.swift
+++ Favorite Locations/ViewController.swift
@@ -8,7 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController ,  UITableViewDelegate, UITableViewDataSource{
+    
+    var activeRow = 0
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = "Row \(indexPath.row)"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+        activeRow = indexPath.row
+        performSegue(withIdentifier: "toSecondViewController", sender:nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toSecondViewController" {
+            let secondViewController = segue.destination as! SecondView
+            secondViewController.activeRow = self.activeRow
+        }
+    }
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
